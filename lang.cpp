ed42311e1f7d3e5f7add8377b07aabda3abf339a
Tom <thoward12@outlook.com>
2017-12-02 12:20:01 -0500

    Class builds reference ascii table

--- lang.cpp
+++ lang.cpp
@@ -8,20 +8,35 @@ using std::string;
 
 int power(int c, int b)
 {
+    // Returns a static cast int from the power function. 
     return static_cast<int>(std::pow(static_cast<float>(c), b));
+}
+
+void Lang::build_table()
+{
+    table = new int[256]();
+    // Handle Spaces
+    table[' '] = 1;
+    
+    // Handle a-z
+    int c = 2;
+    for (int i='a'; i<='z'; i++)
+        table[i] = c++;
+    
 }
 
 Lang::Lang(string language, int n_gram)
 {
-    n = n_gram;
-    len = power(asccii_range, n);
-    freq = new int[len];
-    compute_freq(language);
+    n = n_gram; // N-Gram setting (default 3)
+    len = power(asccii_range, n); // Range is n*n n_gram times
+    freq = new int[len]; // Frequency array
+    compute_freq(language); // Fill the frequency array
 }
 
 Lang::~Lang()
 {
     delete[] freq;
+    delete[] table;
 }
 
 void Lang::print()
