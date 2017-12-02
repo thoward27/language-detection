3431abef1d0fc03255c53b638319bf2871710d52
Tom <thoward12@outlook.com>
2017-12-02 13:18:13 -0500

    Adding test cases to test.cpp

--- test.cpp
+++ test.cpp
@@ -6,6 +6,24 @@
 
 TEST_CASE()
 {
-    Lang l("   ");
-    REQUIRE(l[0] == 0);
+    SECTION("Three spaces")
+    {
+        Lang l("   ");
+        REQUIRE(l[0] == 1);
+    }
+    SECTION("Four Spaces")
+    {
+        Lang l("    ");
+        REQUIRE(l[0] == 2);
+    }
+    SECTION("Three a's")
+    {
+        Lang l("aaa");
+        REQUIRE(l[757] == 1);
+    }
+    SECTION("Three z's")
+    {
+        Lang l("zzz");
+        REQUIRE(l[19682] == 1);
+    }
 }
