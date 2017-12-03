b174c4dce0fcee8e38a41521792827cc14058fbd
Tom <thoward12@outlook.com>
2017-12-03 17:41:55 -0500

    Switched cout to cerr

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 {
     if (argc != 2) 
     {
-        std::cout << "Usage: " << argv[0] << " <STRING>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <STRING>" << std::endl;
         return 1;
     }
 
