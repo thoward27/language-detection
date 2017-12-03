2f7456c982a25d53c2d0fa3acb259ebf9eb0005c
Tom <thoward12@outlook.com>
2017-12-03 14:50:01 -0500

    beginning documentation

--- lang.h
+++ lang.h
@@ -5,12 +5,21 @@
 class Lang
 {
 private:
+    /** The valid ascii range, to compute length of frequencies. 
+     */
     const int ascii_range = 27;
+    /** n signifies what n_gram analysis to provide.
+     */
     const int n;
+    // Len is the length of the interal frequency array. 
     const int len;
+    // The internal frequency array. 
     int* freq;
 
+    // Computes the index for each character, uses ascii base. 
     int power(int c, int b);
+
+
     void compute_freq(std::string language);
 public:
     Lang(std::string language, int n=3);
