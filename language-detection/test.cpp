2c97ab49d8841a3d7ebb6502145f42ba3fea7946
Tom <thoward12@outlook.com>
2017-12-10 11:52:20 -0500

    updated tests to read from files

--- language-detection/test.cpp
+++ language-detection/test.cpp
@@ -6,28 +6,12 @@
 
 TEST_CASE()
 {
-    SECTION("Three Character Tests")
-    {
-        REQUIRE(Lang("   ")[0] == 1);
-        REQUIRE(Lang("aaa")[757] == 1);
-        REQUIRE(Lang("zzz")[19682] == 1);
-    }
-    SECTION(">3 Character Tests")
-    {
-        REQUIRE(Lang("    ")[0] == 2);
-        REQUIRE(Lang("     ")[0] == 3);
-    }
     SECTION("Sentence tests")
     {
-        Lang l("    aaa his zzz");
+        Lang l("data/small_files/test1");
         REQUIRE(l[0] == 2);
         REQUIRE(l[757] == 1);
         REQUIRE(l[19682] == 1);
         REQUIRE(l[6094] == 1);
-    }
-    SECTION("Invalid Init")
-    {
-        REQUIRE_THROWS(Lang("aa12@;;"));
-
     }
 }
